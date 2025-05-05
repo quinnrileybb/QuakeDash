3c7eeacc888d76ea6009b1a2c23e17b7339aa29a
quinnrileybb <162184799+quinnrileybb@users.noreply.github.com>
2025-05-04 21:01:02 -0400

    Update dash.py

--- dash.py
+++ dash.py
@@ -33,7 +33,7 @@ st.success("Welcome to QuakeDash (Princeton).")
 # -------------------------
 # Sidebar Dropdowns for Team/Position/Player Selection
 # -------------------------
-teams = ['PEN_QUA']
+teams = ['PRI_TIG']
 col1, col2, col3 = st.columns(3)
 with col1:
     selected_team = st.selectbox("Select Team", teams)
