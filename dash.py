461bfc3c6a02ef6b521bcfc0b199b3874b91acb4
quinnrileybb <162184799+quinnrileybb@users.noreply.github.com>
2025-06-11 18:08:53 -0400

    Update dash.py

--- dash.py
+++ dash.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.patches import Rectangle
 import seaborn as sns
 
 df = pd.read_csv('penn__5_2__.csv')
